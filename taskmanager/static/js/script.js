b2b2b4f15f389bfbcb3bb9c970354232dbba5d74
seanj06 <seanj06@hotmail.com>
2023-01-16 15:49:11 +0000

    Add add tasks page

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,18 @@ document.addEventListener('DOMContentLoaded', function () {
     // navabar initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
+
+    // datepicker init
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmm, yyyy",
+        i18n: {
+            done: "Select"
+        }
+    });
+
+
+    // select init
+    let selects = document.querySelectorAll('select');
+    M.FormSelect.init(selects);
 });
